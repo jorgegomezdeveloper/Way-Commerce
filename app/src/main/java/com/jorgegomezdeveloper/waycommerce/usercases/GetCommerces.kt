d3990b7b70e439e53c328867fb256a7631d2ff8e
Jorge Gómez <jorgegomezdeveloper@gmail.com>
2021-10-21 13:33:22 +0200

    Logic created  of view model and observer for feature.

--- app/src/main/java/com/jorgegomezdeveloper/waycommerce/usercases/GetCommerces.kt
+++ app/src/main/java/com/jorgegomezdeveloper/waycommerce/usercases/GetCommerces.kt
@@ -1,7 +1,10 @@
 package com.jorgegomezdeveloper.waycommerce.usercases
 
+import androidx.lifecycle.LiveData
+import com.jorgegomezdeveloper.waycommerce.data.network.resource.Resource
 import com.jorgegomezdeveloper.waycommerce.data.repository.WCRepositoryImpl
 
 class GetCommerces(private val wcRepositoryImpl: WCRepositoryImpl) {
 
+    fun execute(): LiveData<Resource<Any>>
 }
